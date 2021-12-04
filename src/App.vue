604ccf76d205f91c0dd8762371d3cbb52a23955a
AztekTR <rubickaghanims@gmail.com>
2021-12-04 17:12:29 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div style="display: flex; flex-direction: column">
+    <ParkingLot :cars="cars" />
+    <ParkingLot :cars="cars2" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ParkingLot from "./components/ParkingLot.vue";
+import { ICar } from "./interfaces/Car";
+
+const cars: ICar[] = [
+  { color: "#1b2e99" },
+  { color: "#1bbe29" },
+  { color: "#1bce89" },
+  { color: "#000" },
+  { color: "#ccc" },
+  { color: "#999" },
+];
+
+const cars2: ICar[] = [
+  { color: "#d94518" },
+  { color: "#0c9ba6" },
+  { color: "#dbdb0d" },
+];
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  background-color: #7a7a7a;
+  height: 100vh;
+  color: #000;
+}
+</style>
---
+++ src/components/ParkingLot.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="parking-lot">
+    <Car v-for="car in cars" :color="car.color" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Car from "./Car.vue";
+import { ICar } from "../interfaces/Car";
+
+defineProps<{ cars: ICar[] }>();
+</script>
+
+<style lang="scss" scoped>
+.parking-lot {
+  width: 850px;
+  border-top: 20px black solid;
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-left: 15px;
+  padding-bottom: 120px;
+  display: flex;
+  color: #1b2e99;
+}
+</style>
