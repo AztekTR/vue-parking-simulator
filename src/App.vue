e169c3dc721d1d5371b422a33e010e40d695575c
Igor Bazyliuk <igor.bazyliuk@vimmi.net>
2021-12-05 19:09:25 +0200

    feat: added devtools and random car generation

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,50 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, reactive } from "vue";
 import ParkingLot from "./components/ParkingLot.vue";
 import { ICar } from "./interfaces/Car";
 
-const cars: ICar[] = [
-  { color: "#1b2e99" },
-  { color: "#1bbe29" },
-  { color: "#1bce89" },
-  { color: "#000" },
-  { color: "#ccc" },
-  { color: "#999" },
+const cars: ICar[] = reactive([
+  { color: "#1b2e99", model: "BMW" },
+  { color: "#1bbe29", model: "BMW" },
+  { color: "#1bce89", model: "BMW" },
+  { color: "#000", model: "BMW" },
+  { color: "#ccc", model: "BMW" },
+  { color: "#999", model: "BMW" },
+]);
+
+const cars2: ICar[] = reactive([
+  { color: "#d94518", model: "BMW" },
+  { color: "#0c9ba6", model: "BMW" },
+  { color: "#dbdb0d", model: "BMW" },
+]);
+
+const generateColor = () =>
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+const models = [
+  "BMW",
+  "Skoda",
+  "Ferrari",
+  "Porsche",
+  "Lada",
+  "Ford",
+  "Honda",
+  "Fiat",
+  "Volvo",
+  "Mercedes",
 ];
 
-const cars2: ICar[] = [
-  { color: "#d94518" },
-  { color: "#0c9ba6" },
-  { color: "#dbdb0d" },
-];
+const pushRandomCar = () =>
+  cars.push({
+    color: generateColor(),
+    model: models[~~[Math.random() * models.length]],
+  });
+
+onMounted(() => {
+  setInterval(pushRandomCar, 10000)
+  });
 </script>
 
 <style>
--- src/interfaces/Car.ts
+++ src/interfaces/Car.ts
@@ -1,3 +1,4 @@
 export interface ICar {
+    model: string,
     color: string;
 }
