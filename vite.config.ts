6df1e64c6e3b10072830ff6e87cdb3e15a697327
AztekTR <rubickaghanims@gmail.com>
2021-12-22 19:07:28 +0200

    feat: github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-parking-simulator/',
   plugins: [vue()],
   server: {
     host: true
